cdf3d3d652fa1bba57c421f8aafb0d8d4dbcb191
Erik Schwarz <Erik.Schwarz@performgroup.com>
2019-09-15 20:56:30 +0200

    files moved

--- src/components/PageLayoutGDPR.js
+++ src/components/PageLayoutGDPR.js
@@ -208,12 +208,12 @@ V Košiciach dňa 27.5.2019
               
               <Divider height="70px" />
               <p className="text_title">Na stiahnutie</p>
-              <a href="/gdpr/Súhlas_so_spracovaním_osobných_údajov.doc">Súhlas so spracovaním osobných údajov</a>
-              <a href="/gdpr/Žiadosť_o_obmedzenie_spracúvania_osobných_údajov.doc">Žiadosť o obmedzenie spracúvania osobných údajov</a>
-              <a href="/gdpr/Žiadosť_o_opravu_a_doplnenie_osobných_údajov.doc">Žiadosť o opravu a doplnenie osobných údajov</a>
-              <a href="/gdpr/Žiadosť_o_poskytnutie_informácií.doc">Žiadosť o poskytnutie informácií</a>
-              <a href="/gdpr/Žiadosť_o_vydanie_osobných_údajov_z_dôvodu_uplatnenia_práva_na_prenosnosť.doc">Žiadosť o vydanie osobných údajov z dôvodu uplatnenia práva na prenosnosť</a>
-              <a href="/gdpr/Žiadosť_o_výmaz_osobných_údajov.doc">Žiadosť o výmaz osobných údajov</a>
+              <a href="/Súhlas_so_spracovaním_osobných_údajov.doc">Súhlas so spracovaním osobných údajov</a>
+              <a href="/Žiadosť_o_obmedzenie_spracúvania_osobných_údajov.doc">Žiadosť o obmedzenie spracúvania osobných údajov</a>
+              <a href="/Žiadosť_o_opravu_a_doplnenie_osobných_údajov.doc">Žiadosť o opravu a doplnenie osobných údajov</a>
+              <a href="/Žiadosť_o_poskytnutie_informácií.doc">Žiadosť o poskytnutie informácií</a>
+              <a href="/Žiadosť_o_vydanie_osobných_údajov_z_dôvodu_uplatnenia_práva_na_prenosnosť.doc">Žiadosť o vydanie osobných údajov z dôvodu uplatnenia práva na prenosnosť</a>
+              <a href="/Žiadosť_o_výmaz_osobných_údajov.doc">Žiadosť o výmaz osobných údajov</a>
             </Grid>
           </Grid>
         </ServicesMargin>
